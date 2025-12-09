3343782ec83696028949f8c45555e434fc9f8d0a
Hyunwook Go <hyunwook.go@concentrix.com>
2025-12-09 10:58:43 +0900

    feat: Init project

---
+++ src/test/kotlin/com/resilient/processor/ResilientTrafficProcessorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.resilient.processor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ResilientTrafficProcessorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
